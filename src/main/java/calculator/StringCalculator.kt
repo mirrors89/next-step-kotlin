077495e09ae313f3e06752cdb83e39ccbf73fbaa
이기승 <keeseung@sixshop.com>
2020-02-16 17:08:03 +0900

    2.4 테스트와 리팩토링을 통한 문자열 계산기 구현 - 코드 리팩토링 - 메소드가 하나의 일만 하도록 분리

--- src/main/java/calculator/StringCalculator.kt
+++ src/main/java/calculator/StringCalculator.kt
@@ -1,16 +1,29 @@
 package calculator
 
+import kotlin.streams.toList
+
+
 class StringCalculator {
     fun add(text: String?): Int {
-        if (text == null || text.isEmpty()) {
+        if (isBlank(text)) {
             return 0
         }
 
-        val values = text.split(",")
+        return sum(toInts(split(text)))
+    }
+
+    private fun split(text: String?) = text!!.split(",")
+
+    private fun isBlank(text: String?) = text == null || text.isEmpty()
+
+    private fun toInts(values: List<String>): List<Int> =
+            values.stream().map { value -> Integer.parseInt(value) }.toList()
+
+    private fun sum(values: List<Int>): Int {
         var sum = 0
 
-        for(value in values) {
-            sum += Integer.parseInt(value)
+        for (value in values) {
+            sum += value
         }
         return sum
     }
