53626cd0e9c03a5ff2e2154b58ba1c6103ba5049
이기승 <keeseung@sixshop.com>
2020-02-16 15:28:48 +0900

    2.1 main() 메소드를 활용한 테스트의 문제점 - 3

--- src/main/java/CalculatorTest.java
+++ src/main/java/CalculatorTest.java
@@ -4,9 +4,30 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        System.out.println(calculator.add(3, 4));
-        System.out.println(calculator.subtract(5, 4));
-        System.out.println(calculator.multiply(2, 6));
-        System.out.println(calculator.divide(8, 4));
+        add(calculator);
+        subtract(calculator);
+        multiply(calculator);
+        divide(calculator);
     }
+
+    private static void add(Calculator calculator) {
+        System.out.println(calculator.add(3, 4));
+
+    }
+
+    private static void subtract(Calculator calculator) {
+        System.out.println(calculator.subtract(5, 4));
+
+    }
+
+    private static void multiply(Calculator calculator) {
+        System.out.println(calculator.multiply(2, 6));
+
+    }
+
+    private static void divide(Calculator calculator) {
+        System.out.println(calculator.divide(8, 4));
+
+    }
+
 }
