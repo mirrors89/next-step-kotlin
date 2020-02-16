cb3376cb64c51b755804402b8d574cc187bf29e4
이기승 <keeseung@sixshop.com>
2020-02-16 15:36:50 +0900

    2.2 Junit을 활용해 main() 메소드 문제점 극복 - 4

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,16 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-    private Calculator calculator = new Calculator();
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        calculator = new Calculator();
+    }
 
     @Test
     public void add() {
