e8c91ab9b147fd2f5a2462782ea34aad14f52e6e
이기승 <keeseung@sixshop.com>
2020-02-16 15:35:51 +0900

    2.2 Junit을 활용해 main() 메소드 문제점 극복 - 3

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-
+    private Calculator calculator = new Calculator();
 
     @Test
     public void add() {
-        Calculator calculator = new Calculator();
         assertEquals(9, calculator.add(6, 3));
 
     }
     @Test
     public void subtract() {
-        Calculator calculator = new Calculator();
         assertEquals(3, calculator.subtract(6, 3));
     }
 
     @Test
     public void multiply() {
-        Calculator calculator = new Calculator();
         assertEquals(12, calculator.multiply(2, 6));
     }
 
     @Test
     public void divide() {
-        Calculator calculator = new Calculator();
         assertEquals(2, calculator.divide(8, 4));
     }
 
