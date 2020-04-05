68bba93c527795ccd5b66bbea169017c1198c458
이기승 <keeseung@sixshop.com>
2020-04-05 16:52:47 +0900

    6장 서블릿 실습전 불필요한 코드 제거

--- src/main/java/webserver/controller/Controller.kt
+++
@@ -1,8 +0,0 @@
-package webserver.controller
-
-import webserver.http.HttpRequest
-import webserver.http.HttpResponse
-
-interface Controller {
-    fun service(httpRequest: HttpRequest, httpResponse: HttpResponse)
-}
