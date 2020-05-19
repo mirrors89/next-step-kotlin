3841eecab7a46df4b595df15bcc9e3edaa880c86
이기승 <keeseung@sixshop.com>
2020-05-19 21:04:19 +0900

    10.1 DateMessageProvider DI 적용

--- src/test/java/util/DateMessageProvider.kt
+++ src/test/java/util/DateMessageProvider.kt
@@ -3,12 +3,10 @@ package util
 import java.util.*
 
 class DateMessageProvider {
-    val dateMessage: String
-        get() {
-            val now = Calendar.getInstance()
-            val hour = now[Calendar.HOUR_OF_DAY]
-            return if (hour < 12) {
-                "오전"
-            } else "오후"
-        }
+    fun getDateMessage(now: Calendar): String {
+        val hour = now[Calendar.HOUR_OF_DAY]
+        return if (hour < 12) {
+            "오전"
+        } else "오후"
+    }
 }
--- src/test/java/util/DateMessageProviderTest.kt
+++ src/test/java/util/DateMessageProviderTest.kt
@@ -1,21 +1,29 @@
 package util
 
-import org.junit.jupiter.api.Assertions.*
-
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.util.*
 
 internal class DateMessageProviderTest {
 
     @Test
     fun 오전() {
         val provider = DateMessageProvider()
-        assertEquals("오전", provider.dateMessage)
+        assertEquals("오전", provider.getDateMessage(createCurrentDate(11)))
     }
-
 
     @Test
     fun 오후() {
         val provider = DateMessageProvider()
-        assertEquals("오후", provider.dateMessage)
+        assertEquals("오후", provider.getDateMessage(createCurrentDate(13)))
     }
+
+
+
+    private fun createCurrentDate(hourOfDay: Int): Calendar {
+        val now = Calendar.getInstance()
+        now.set(Calendar.HOUR_OF_DAY, hourOfDay)
+        return now
+    }
+
 }
