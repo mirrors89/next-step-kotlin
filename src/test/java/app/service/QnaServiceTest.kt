7ab4278509b1f5b0e6f8d7a5e60eb4699fc546ea
이기승 <keeseung@sixshop.com>
2020-05-21 20:31:17 +0900

    11.3 Mockito를 활용한 테스트

--- src/test/java/app/service/QnaServiceTest.kt
+++ src/test/java/app/service/QnaServiceTest.kt
@@ -2,33 +2,40 @@ package app.service
 
 import app.CannotDeleteException
 import app.dao.AnswerDao
-import app.dao.MockAnswerDao
-import app.dao.MockQuestionDao
 import app.dao.QuestionDao
 import app.model.Question
 import app.model.User
-import org.junit.jupiter.api.Assertions.*
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeEach
-
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(MockKExtension::class)
 internal class QnaServiceTest {
 
     private lateinit var qnaService: QnaService
+
+    @RelaxedMockK
     private lateinit var questionDao: QuestionDao
+    @RelaxedMockK
     private lateinit var answerDao: AnswerDao
 
 
     @BeforeEach
     fun setup() {
-        questionDao = MockQuestionDao()
-        answerDao = MockAnswerDao()
         qnaService = QnaService(questionDao, answerDao)
     }
 
     @Test
     fun deleteQuestion_없는_질문() {
         assertThrows(CannotDeleteException::class.java) {
+
+            every { questionDao.findById(1L) }
+                    .returns(null)
+
             qnaService.deleteQuestion(1L, newUser("userId"))
         }
 
@@ -37,8 +44,9 @@ internal class QnaServiceTest {
     @Test
     fun deleteQuestion_다른_사용자() {
         assertThrows(CannotDeleteException::class.java) {
-            var question = newQuestion(1L, "keeseung");
-            questionDao.insert(question)
+            val question = newQuestion(1L, "keeseung")
+            every { questionDao.findById(1L) }.returns(question)
+            every { answerDao.findAllByQuestionId(1L) }.returns(arrayListOf())
             qnaService.deleteQuestion(1L, newUser("userId"))
         }
 
@@ -46,8 +54,9 @@ internal class QnaServiceTest {
 
     @Test
     fun deleteQuestion_같은_사용자_답변없음() {
-        var question = newQuestion(1L, "keeseung");
-        questionDao.insert(question)
+        val question = newQuestion(1L, "keeseung")
+        every { questionDao.findById(1L) }.returns(question)
+        every { answerDao.findAllByQuestionId(1L) }.returns(arrayListOf())
         qnaService.deleteQuestion(1L, newUser("keeseung"))
     }
 
